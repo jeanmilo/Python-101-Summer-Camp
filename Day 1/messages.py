95156689d9277c671e1bba937b35631d840c57d7
Claudia (Dia) <claudia.parlee@gmail.com>
2025-03-13 11:48:31 -0400

    Create messages.py

---
+++ Day 1/messages.py
@@ -0,0 +1,8 @@
+# Name: Dia Yamamoto
+# Purpose: this program is going to input and output a message from the user
+
+message = input("Enter a message: ")
+
+print(message)
+print(message.lower())
+print(message.upper())
